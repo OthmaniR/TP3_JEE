dab3adbc6662d9b0a90c94b9d1c7b43c8214cb53
Rayen <rayenothmani@gmail.com>
2023-03-02 23:27:47 +0100

    Tp3 commit

---
+++ src/main/java/test/TestMetier.java
@@ -0,0 +1,18 @@
+package test;
+import java.util.List;
+
+import metier.MetierImpl;
+import metier.Produit;
+
+public class TestMetier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MetierImpl metier= new MetierImpl();
+		List<Produit> prods = metier.getProduitsParMotCle("Hp");
+		for (Produit p : prods)
+		System.out.println(p.getNomProduit());
+
+	}
+
+}
